99aa6af0534ed7e17019b9bb8eb6fa9d8cfd48ca
Jesus Marenco <jesusmarencodev@gmail.com>
2023-06-26 10:38:04 -0500

    implement formik-components

--- src/pages/FormikComponents.tsx
+++ src/pages/FormikComponents.tsx
@@ -1,56 +1,73 @@
-import { useFormik } from "formik";
+import { useFormik, Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import "../styles/styles.css";
 
 const FormikComponents = () => {
-  const {
-    handleSubmit,
-    errors,
-    touched,
-    getFieldProps,
-  } = useFormik({
-    initialValues: {
-      firstName: "",
-      lastName: "",
-      email: "",
-    },
-    onSubmit: (values) => {
-      console.log(JSON.stringify(values, null, 2));
-    },
-    validationSchema: Yup.object({
-      firstName: Yup.string()
-        .max(15, "Debe de tener 15 caracteres o menos")
-        .required("Requerido"),
-      lastName: Yup.string()
-        .max(10, "Debe de tener 10 caracteres o menos")
-        .required("Requerido"),
-      email: Yup.string()
-        .email("Email no tiene un formato valido")
-        .required("Requerido"),
-    }),
-  });
-
   return (
     <div>
-      <h1>Formik Yup Page</h1>
-      <form noValidate onSubmit={handleSubmit}>
-        <label htmlFor="firstName">First Name</label>
-        <input type="text" {...getFieldProps("firstName")} />
-        {touched.firstName && errors.firstName && (
-          <span>{errors.firstName}</span>
+      <h1>FormikComponents</h1>
+
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          terms: false,
+          jobType: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+        validationSchema={Yup.object({
+          firstName: Yup.string()
+            .max(15, "Debe de tener 15 caracteres o menos")
+            .required("Requerido"),
+          lastName: Yup.string()
+            .max(10, "Debe de tener 10 caracteres o menos")
+            .required("Requerido"),
+          email: Yup.string()
+            .email("Email no tiene un formato valido")
+            .required("Requerido"),
+          terms: Yup.boolean().oneOf([true], "Debe de aceptar las condiciones"),
+          jobType: Yup.string()
+            .required("Requerido")
+            .notOneOf(["it-jr"], "Esta opcion no esta permitida"),
+        })}
+      >
+        {(formik) => (
+          <Form>
+            <label htmlFor="firstName">First Name</label>
+            <Field name="firstName" type="text" />
+            <ErrorMessage component="span" name="firstName" />
+
+            <label htmlFor="lastName">Last Name</label>
+            <Field name="lastName" type="text" />
+            <ErrorMessage component="span" name="lastName" />
+
+            <label htmlFor="email">Email</label>
+            <Field name="email" type="text" />
+            <ErrorMessage component="span" name="email" />
+
+            <label htmlFor="jobType">Job Type</label>
+            <Field name="jobType" as="select">
+              <option value="">Pick something</option>
+              <option value="developer">Developer</option>
+              <option value="designer">Designer</option>
+              <option value="it-senior">It-senior</option>
+              <option value="it-jr">It-junior</option>
+            </Field>
+            <ErrorMessage component="span" name="jobType" />
+
+            <label htmlFor="terms">
+              <Field name="terms" type="checkbox" />
+              Terms and conditions
+            </label>
+            <ErrorMessage component="span" name="terms" />
+
+            <button type="submit"> Sent </button>
+          </Form>
         )}
-
-        <label htmlFor="lastName">Last Name</label>
-        <input type="text" {...getFieldProps("lastName")} />
-        {touched.lastName && errors.lastName && <span>{errors.lastName}</span>}
-
-        <label htmlFor="email">Email</label>
-        <input {...getFieldProps("email")} type="email" />
-        {touched.email && errors.email && <span>{errors.email}</span>}
-
-        <button type="submit"> Sent </button>
-        <button> Reset Form </button>
-      </form>
+      </Formik>
     </div>
   );
 };
